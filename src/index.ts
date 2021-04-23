af1185f2614a803d6cad41bc8acf8b16e2974d98
Goutam Das <goutam@stockedge.com>
2021-04-23 23:29:23 +0530

    API: get all books

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import "dotenv/config";
 import Express from "express";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
+import { SERVER_PORT, MONGO_URL, MONGO_OPTIONS } from "./config";
 import bookRoutes from "./routes/book";
 
 (async () => {
   try {
+    await mongoose.connect(MONGO_URL, MONGO_OPTIONS);
+    console.log("MongoDB connected!");
     const app = Express();
     app.use("/api/books", bookRoutes);
     app.get("/", (_, res) => {
       res.send("Hello");
     });
-    app.listen(process.env.PORT, () => {
-      console.log(`Server listening on ${process.env.PORT}`);
+    app.listen(SERVER_PORT, () => {
+      console.log(`Server listening on ${SERVER_PORT}`);
     });
   } catch (error) {
     console.error(error);
