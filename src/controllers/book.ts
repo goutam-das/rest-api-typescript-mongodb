63581f3cdd0dadc63acf81f4cb1a18300c62868d
Goutam Das <goutam@stockedge.com>
2021-04-23 22:36:15 +0530

    Book route setup

---
+++ src/controllers/book.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+const getAllBooks = (req: Request, res: Response) => {};
+
+const getBook = (req: Request, res: Response) => {};
+
+const addBook = (req: Request, res: Response) => {};
+
+const updateBook = (req: Request, res: Response) => {};
+
+const deleteBook = (req: Request, res: Response) => {};
+
+export { getAllBooks, getBook, addBook, updateBook, deleteBook };
